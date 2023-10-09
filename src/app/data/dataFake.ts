d5121c5290dab5ba81a1478e366893aadb1de673
Pollyana Masiero Torquato <pollyanamasiero@hotmail.com>
2023-10-09 10:51:38 -0300

    project completed

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,28 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title":"NOVO HOMEM DE FERRO ANUNCIADO",
+    "description":"Marvel anuncia data para lançamento do novo filme do homem de ferro",
+    "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/80C64C0B63382CD3ED2653356125F275F63D036028A77D38DC3286AD851A6833/scale?width=1200&aspectRatio=1.78&format=jpeg"
+  },
+  {
+    "id":"2",
+    "title":"Novos lançamentos são aguardados para o próximo semestre",
+    "description":"Produções da quinta fase do MCU chegam aos cinemas e televisão no próximo ano.",
+    "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+  },
+  {
+    "id":"3",
+    "title":"Loki | Dos quadrinhos para as telas, vilão ou herói?",
+    "description":"Loki é o Príncipe da Maldade, Deus da Mentira e poderoso demais pra ser influenciado por um simples cetro.",
+    "photoCover":"https://t.ctcdn.com.br/ZSRwUv3JcX2-ako-BByg8NVfcJg=/640x360/smart/i808139.jpeg"
+  },
+  {
+    "id":"4",
+    "title":"Vingadores: Ultimato (2019) - US$ 858 milhões",
+    "description":"Vingadores: Ultimato (2019) foi o filme de maior sucesso de bilheteria da Marvel",
+    "photoCover":"https://tm.ibxk.com.br/2022/05/04/04181551056727.jpg?ims=1200x675"
+  }
+]
+
+
